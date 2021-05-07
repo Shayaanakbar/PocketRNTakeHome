31c1855f85be3a90d51da376fe73facb737b3097
ShayaanAkbar <shayaanakbar1106@gmail.com>
2021-05-07 16:47:34 -0400

    read documentation wrong fixing a couple of authentication glitches to send everything to dashboard

--- hosting/src/pages/SignIn.jsx
+++ hosting/src/pages/SignIn.jsx
@@ -1,5 +1,7 @@
 // Bootstrapped SignIn from MaterialUI
 import React, { useState } from 'react';
+import { withFirebase } from "../firebaseFE/index";
+import { Link, withRouter } from 'react-router-dom'
 
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
@@ -7,7 +9,6 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
@@ -114,4 +115,4 @@ function SignIn(props) {
     );
 }
 
-export default SignIn
+export default withRouter(withFirebase(SignIn));
