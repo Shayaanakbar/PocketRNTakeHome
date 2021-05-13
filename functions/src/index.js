7a8536911f6556ae5ed1dd88d518949a30a77799
ShayaanAkbar <shayaanakbar1106@gmail.com>
2021-05-13 14:50:43 -0400

    created new user and user deleted functions. Testing console long both are showing up on console

--- functions/src/index.js
+++ functions/src/index.js
@@ -2,9 +2,19 @@ const functions = require("firebase-functions");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 
+// Hello world request check firebase functions are working
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+// firebase auth trigger (new user signup)
+exports.newUsersSignup = functions.auth.user().onCreate(user => {
+  console.log('user created', user.email, user.uid)
+});
+
+// firebase auth trigger (new user delete)
+exports.userDeleted = functions.auth.user().onDelete(user => {
+  console.log('user deleted', user.email, user.uid);
+})
